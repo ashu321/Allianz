6912730ba908c5df4793c354009893bdf2a6ecad
ashishtomar <capsys31@yahoo.com>
2022-08-03 17:06:04 +0530

    Allianz Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AllianzService } from './allianz.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-allianz';
+export class AppComponent implements OnInit{
+  
+  title = 'Allianz Assignment';
+  countryList:any;
+  countryname:string;
+  filteredCountryList:any;
+
+  constructor(private allianzService:AllianzService){
+
+  }
+  ngOnInit(): void {
+    this.allianzService.getCountryList().subscribe(data=>{
+      this.countryList = data;
+      this.assignCopy();
+      console.log(data);
+    });
+  }
+
+  assignCopy(){
+    this.filteredCountryList = Object.assign([], this.countryList);
+ }
+ filterItem(_event){
+    let value = _event.target.value;
+    if(!value){
+        this.assignCopy();
+    } // when nothing has typed
+    this.filteredCountryList = Object.assign([], this.countryList).filter(
+       item => item.name.common.toLowerCase().indexOf(value.toLowerCase()) > -1
+    )
+ }
+
 }
